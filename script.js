db1aeb23d480a84cb44367054097454bea41f842
sontt <thanhsonl2c@gmail.com>
2024-01-03 14:03:19 +0700

    js push

--- script.js
+++ script.js
@@ -5,23 +5,38 @@
 // console.log('See noS');
 
 /* 3: JS data types */
-let a = 5;
+// let a = 5;
 // let b = 5.5;
 // let c = 6, d = 7, e = 8;
 // console.log("Check variable:", a, b, c);
 // console.log("Check variable:", 'a =', a, 'b =', b, 'c =',c);
 
 /* 4: Strings */
-let strings = "Strings";
+// let strings = "Strings";
 // console.log(strings);
 // Note: JavaScript evaluates expressions from left to right
 // console.log(1 + 1 + 'a'); // 2 + 'a' = 2a
 // console.log('a' + 1 + 1); // a1 + 1 = a11
 // let text = `I see "noS"`; ``, '', ""
 
+/* 5: Method with strings */
+// https://www.w3schools.com/js/js_string_methods.asp
 
-/* 5:  */
-/* */
+/* 6: Numbers */
+// let a = 2;
+// let b = 3;
+// let c = 4;
+// let d = '5';
+// let e = +5; // convert strings to number
+// let f = Number(e);
+// console.log(a+b);
+// console.log(a+c);
+// console.log(a*d);
+// console.log(a-d);
+// console.log(d, typeof d);
+// console.log(e, typeof e);
+// console.log(f, typeof f);
+
 /* */
 /* */
 /* */
