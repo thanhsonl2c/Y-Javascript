5fd6886aaaf36f1980a0f04e2a998ceb9b16d520
sontt <thanhsonl2c@gmail.com>
2024-01-03 09:12:37 +0700

    add file

---
+++ script.js
@@ -0,0 +1,49 @@
+/* 1: Alert */
+// alert('See noS');
+
+/* 2: Console */
+// console.log('See noS');
+
+/* 3: JS data types */
+let a = 5;
+// let b = 5.5;
+// let c = 6, d = 7, e = 8;
+// console.log("Check variable:", a, b, c);
+// console.log("Check variable:", 'a =', a, 'b =', b, 'c =',c);
+
+/* 4: Strings */
+let strings = "Strings";
+// console.log(strings);
+// Note: JavaScript evaluates expressions from left to right
+// console.log(1 + 1 + 'a'); // 2 + 'a' = 2a
+// console.log('a' + 1 + 1); // a1 + 1 = a11
+// let text = `I see "noS"`; ``, '', ""
+
+
+/* 5:  */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
